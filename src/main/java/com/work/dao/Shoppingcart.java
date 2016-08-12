c5fe4c36557c8ec5d9418fa908fe30b537624d8d
linzhi <739769483@qq.com>
2016-08-12 21:58:07 +0800

    添加购物车模块， 粗略完成该模块的CURD语句的书写

---
+++ src/main/java/com/work/dao/Shoppingcart.java
@@ -0,0 +1,10 @@
+package com.work.dao;
+
+import com.work.util.BaseMapper;
+
+/**
+ * Created by 林智 on 2016/8/12.
+ */
+public interface Shoppingcart extends BaseMapper<Shoppingcart> {
+
+}
---
+++ src/main/java/com/work/model/Shoppingcart.java
@@ -0,0 +1,73 @@
+package com.work.model;
+
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.Date;
+
+/**
+ * Created by 林智 on 2016/8/12.
+ */
+@Table(name="shoppingcart")
+public class Shoppingcart {
+    private int s_id;
+    private int goods_id;
+    private int user_id;
+    private int piece;
+    private Date s_time;
+
+    @Transient
+    private Goods goods;
+    @Transient
+    private User user;
+
+    @Override
+    public String toString() {
+        return "Shoppingcart{" +
+                "s_id=" + s_id +
+                ", goods_id=" + goods_id +
+                ", user_id=" + user_id +
+                ", piece=" + piece +
+                ", s_time=" + s_time +
+                '}';
+    }
+
+    public int getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(int s_id) {
+        this.s_id = s_id;
+    }
+
+    public int getGoods_id() {
+        return goods_id;
+    }
+
+    public void setGoods_id(int goods_id) {
+        this.goods_id = goods_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getPiece() {
+        return piece;
+    }
+
+    public void setPiece(int piece) {
+        this.piece = piece;
+    }
+
+    public Date getS_time() {
+        return s_time;
+    }
+
+    public void setS_time(Date s_time) {
+        this.s_time = s_time;
+    }
+}
